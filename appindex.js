e05b939d60f01f1a87bf2916c0ab0ed53976655e
hacker-guy <bugeeja@gmail.com>
2018-12-28 19:27:16 +1100

    improved scroll and added support for multiple countries

--- appindex.js
+++ appindex.js
@@ -1,35 +1,32 @@
 
 const INIT_N = 10;
 const handler =  _.throttle(checkScroll, 500);
-var page = 1;
-
-window.addEventListener("scroll", handler);
+var page = 1, countrycode = "";
+var listener = false;
 
 function checkScroll() {
-	if($(window).scrollTop() + $(window).height() >= $(document).height()*0.75) {
+	if($(window).scrollTop() + $(window).height() >= $(document).height()*0.85) {
 	   	/* Update content when scrolling */
 	   	page += 1;
-	  	 aLoad();
+	  	aLoad();
 	}
 }
 
 function aLoad() {
-	// var hrefloc = $(location).attr('href');
-	// var page = hrefloc.substr(hrefloc.lastIndexOf('/') + 1);
 	var jsonObj = ''
-	// if (page == "index.html") {
-	jsonObj = "https://newsapi.org/v2/top-headlines?country=au&pagesize=10&page=" + page + "&apiKey=4cdd18177876469f965b824dda3ec945";
-	// } else if (page == "source.html") {
-	// 	jsonObj = "https://newsapi.org/v2/sources?apiKey=4cdd18177876469f965b824dda3ec945";
-	// } else if (page == "category.html") {
-
+	console.log(countrycode);
+	console.log(page);
+	jsonObj = "https://newsapi.org/v2/top-headlines?country=" + countrycode + "&pagesize=10&page=" + page + "&apiKey=4cdd18177876469f965b824dda3ec945";
 	$.getJSON(jsonObj, 
 		function(json) {
 			console.log(json);
 			if (json != "Nothing found.") {
 				if (json.articles.length == 0) {
-					window.removeEventListener("scroll", handler);
-					$(".articles").append("<br>That's all folks!");
+					if (listener == true) {
+						window.removeEventListener("scroll", handler);
+						listener = false;
+						$(".articles").append("<br>That's all folks!");
+					}
 				}
 				addArticles(INIT_N, json);
 			}
@@ -64,5 +61,22 @@ function addArticles(n, json) {
 }
 
 $(function() {
-	aLoad();
+	var hrefloc = $(location).attr('href');
+	var pageon = hrefloc.substr(hrefloc.lastIndexOf('/') + 1);
+	window.addEventListener("scroll", handler);
+	listener = true;
+	if (pageon == "index.html") {
+		countrycode = "au";
+		aLoad();
+	} else if (pageon == "country.html") {
+	 	$('.dropbtn').bind('click', function() {
+	 		$(".articles").html("")
+	 		if (listener == false) {
+	 			window.addEventListener("scroll", handler);
+	 			listener = true;
+	 		}
+	 		page = 1;
+	 		countrycode = $('select').children("option:selected").val();
+			aLoad()});
+	}
 });
